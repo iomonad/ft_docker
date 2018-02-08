88f4974e14ade15bdc2502b14c3abf595cf4b93a
Clement Trosa <me@trosa.io>
2018-02-08 08:23:22 +0100

    better grepping

--- 01_dockerfiles/ex03/.assets/gitlab.rb
+++
@@ -1,7 +0,0 @@
-postgresql['shared_buffers'] = '1MB'
-manage_accounts['enable'] = false
-host = `hostname`.strip
-external_url "http://#{host}"
-eval ENV["GITLAB_OMNIBUS_CONFIG"].to_s
-from_file("/etc/gitlab/gitlab.rb")
-unicorn['worker_processes'] = 2
